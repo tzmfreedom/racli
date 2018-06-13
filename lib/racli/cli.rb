976a7a1fe4dba8841950c9ccb48512e3533e2842
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2018-06-13 22:00:30 +0900

    Add aliases, default_headers

--- lib/racli/cli.rb
+++ lib/racli/cli.rb
@@ -6,23 +6,30 @@ Dir[File.expand_path('./handlers/**/*.rb', __dir__)].each { |f| require f }
 
 module Racli
   class CLI
-    attr_accessor :method, :path, :params, :config
-
     def initialize(config:, rcfile:)
       config_ruby_string = File.read(config)
       @rackapp = eval "::Racli::Rack.new { #{config_ruby_string} }.to_app", TOPLEVEL_BINDING, '(racli)', 0
       @default_handler = Racli::Handlers::DefaultHandler.new(self)
+      @handlers = []
+      @aliases = {}
+      @default_headers = {}
 
       eval(File.read(rcfile)) if File.exists?(rcfile)
     end
 
     def call(method:, path:, params:)
+      if @aliases.include?(method.to_sym)
+        alias_setting = @aliases[method.to_sym]
+        method = alias_setting[:method]
+        path = alias_setting[:path]
+      end
+
       request_params = request_params(method: method, path: path, params: params)
       status, headers, body = @rackapp.call(request_params)
 
       original_args = { method: method, path: path, params: params }
       catch(:abort) do
-        response_handlers = handlers + [@default_handler]
+        response_handlers = @handlers + [@default_handler]
         response_handlers.reduce([status, headers, body]) do |(status, headers, body), handler|
           handler.call(status, headers, body, original_args)
         end
@@ -31,7 +38,15 @@ module Racli
 
     def add_handler(handler_klass)
       handler = handler_klass.new(self)
-      handlers.push handler
+      @handlers.push handler
+    end
+
+    def add_alias(alias_name, method = 'GET', path = '/')
+      @aliases[alias_name] = { method: method, path: path }
+    end
+
+    def default_headers(headers)
+      @default_headers = headers
     end
 
     private
@@ -41,7 +56,8 @@ module Racli
       request_params = {
         'PATH_INFO' => path || '/',
         'REQUEST_METHOD' => method || 'GET',
-      }
+      }.merge!(@default_headers)
+
       if method == 'GET'
         request_params['QUERY_STRING'] = query_string
         request_params['rack.input'] = StringIO.new('')
@@ -50,10 +66,6 @@ module Racli
         request_params['rack.input'] = StringIO.new(query_string)
       end
       request_params
-    end
-
-    def handlers
-      @handlers ||= []
     end
 
     def to_query_string(params)
